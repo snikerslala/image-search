dc58c1fc6b4544d003e59a43dcc64908773e8f2d
Martyna Skup <martyna.skup@yahoo.de>
2019-06-28 14:00:05 +0200

    Get data from the Unsplash API based on searched term

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,12 @@ import SearchBox from './SearchBox';
 
 class App extends Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com//search/photos', {
+          params: {query: term},  
+          headers: {
+              Authorization: 'Client-ID 5ac241e6d320313fd8f62a929dd23c9c71a5ccc7d1f9b88d6b1dfe480168ef0d'
+              }
+        });
     }
 
     render() {
