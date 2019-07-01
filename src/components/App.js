fa83ac8f59690e2aa83aae8dbe8055e5122b8309
Martyna Skup <martyna.skup@yahoo.de>
2019-07-01 20:49:16 +0200

    Create unsplash.js to hold the axios configuration for get requests

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBox from './SearchBox';
 
 class App extends Component {
     state = {images : []};
 
     onSearchSubmit = async (term) => {
-        const response = await axios.get('https://api.unsplash.com/search/photos', {
-          params: {query: term},  
-          headers: {
-              Authorization: 'Client-ID 5ac241e6d320313fd8f62a929dd23c9c71a5ccc7d1f9b88d6b1dfe480168ef0d'
-              }
+        const response = await unsplash.get('/search/photos', {
+          params: {query: term},
         });
     this.setState( {images: response.data.results });
     }
