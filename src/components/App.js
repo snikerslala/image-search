9e218d155da3b663060880164f9cba7f1c83a0aa
Martyna Skup <martyna.skup@yahoo.de>
2019-07-01 19:44:35 +0200

    Set state for images
    1. Set the state for images after an asynch request
    2. Show the item number of received array
    3. Change the onSearchSubmit function to an arrow function and assign it to the function name

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,23 @@ import axios from 'axios';
 import SearchBox from './SearchBox';
 
 class App extends Component {
-    async onSearchSubmit(term) {
+    state = {images : []};
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com//search/photos', {
           params: {query: term},  
           headers: {
               Authorization: 'Client-ID 5ac241e6d320313fd8f62a929dd23c9c71a5ccc7d1f9b88d6b1dfe480168ef0d'
               }
         });
-    console.log(response.data.results);
+    this.setState( {images: response.data.results });
     }
 
     render() {
         return (
         <div className="ui container" style={{ marginTop: '10px', textAlign: 'center' }}>
             <SearchBox onSubmit={this.onSearchSubmit}/>
+            Found: {this.state.images.length} images
         </div>
         )
     }
