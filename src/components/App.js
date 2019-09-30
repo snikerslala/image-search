a6424465117060265a17c6b9823a01e6bb8ae009
Martyna Skup <46429607+snikerslala@users.noreply.github.com>
2019-09-30 15:35:45 +0200

    Update the loader with messages
    1. Update cases when messages should appear on the screen before the first search, while loading the results and if there were no search results provided from the API
    2. Add state firstRequest to show the entry message, before the first search has been requested

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,18 @@ import SearchBox from './SearchBox';
 import Loader from './Loader';
 
 class App extends Component {
-  state = { images: [], loading: false };
+  state = { images: [], loading: false, firstRequest: true };
 
   onSearchSubmit = async term => {
-    if (!this.state.images.length) {
-      this.setState({ loading: true });
-    }
-
+    this.setState({ firstRequest: false });
+    this.setState({ loading: true });
     const response = await unsplash.get('/search/photos', {
       params: { query: term }
     });
 
     this.setState({ images: response.data.results });
+    this.setState({ loading: false });
+    console.log(this.state.firstRequest);
   };
 
   render() {
@@ -25,7 +25,11 @@ class App extends Component {
         style={{ marginTop: '10px', textAlign: 'center' }}
       >
         <SearchBox onSubmit={this.onSearchSubmit} />
-        <Loader images={this.state.images} loading={this.state.loading} />
+        <Loader
+          images={this.state.images}
+          loading={this.state.loading}
+          firstRequest={this.state.firstRequest}
+        />
       </div>
     );
   }
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ImageList from './ImageList';
 
-const Loader = ({ images, loading }) => {
-  if (!images.length && !loading) {
+const Loader = ({ images, loading, firstRequest }) => {
+  if (firstRequest) {
     return (
       <div
         className="ui small message"
@@ -13,7 +13,7 @@ const Loader = ({ images, loading }) => {
     );
   }
 
-  if (!images.length && loading) {
+  if (loading) {
     return (
       <div
         className="ui icon message"
@@ -22,8 +22,18 @@ const Loader = ({ images, loading }) => {
         <i className="notched circle loading icon"></i>
         <div className="content">
           <div className="header">Just a second...</div>
-          <p>I'm loading pictures for you!</p>
+          <p>I'm searching pictures for you!</p>
         </div>
+      </div>
+    );
+  }
+  if (!loading && !firstRequest && !images.length) {
+    return (
+      <div
+        className="ui small message"
+        style={{ width: '300px', margin: 'auto' }}
+      >
+        Sorry, we didn't find any pictures for your request. Please try again!
       </div>
     );
   }
